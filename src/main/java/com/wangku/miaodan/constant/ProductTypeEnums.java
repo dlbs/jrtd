445e33b0f6e874dfc75779d1824405cf1ebd84f0
dlmarker <3026159710@qq.com>
2019-03-14 14:01:14 +0800

    代码提交

--- src/main/java/com/wangku/miaodan/constant/ProductTypeEnums.java
+++ src/main/java/com/wangku/miaodan/constant/ProductTypeEnums.java
@@ -5,12 +5,16 @@ import java.math.BigDecimal;
 
 public enum ProductTypeEnums {
 	
-	PRODUCT_ONE("0精品/30淘单", 1, 0, 30, new BigDecimal(688)),
+	PRODUCT_ONE("13精品", 1, 13, 0, new BigDecimal(500)),
+	PRODUCT_TWO("30精品", 2, 30, 0, new BigDecimal(1000)),
+	PRODUCT_THREE("70精品", 3, 70, 0, new BigDecimal(2000));
+	
+/*	PRODUCT_ONE("0精品/30淘单", 1, 0, 30, new BigDecimal(688)),
 	PRODUCT_TWO("15精品/0淘单", 2, 15, 0, new BigDecimal(1688)),
 	PRODUCT_THREE("32精品/15淘单", 3, 30, 15, new BigDecimal(2888)),
 	PRODUCT_FOUR("66精品/30淘单", 4, 66, 30, new BigDecimal(5688)),
 	PRODUCT_FIVE("102精品/30淘单", 5, 102, 30, new BigDecimal(8088)),
-	PRODUCT_SIX("153精品/30淘单", 6, 153, 30, new BigDecimal(10888));
+	PRODUCT_SIX("153精品/30淘单", 6, 153, 30, new BigDecimal(10888));*/
 	
 	private String desc;
 	
@@ -35,15 +39,9 @@ public enum ProductTypeEnums {
 			return PRODUCT_ONE;
 		} else if (type == 2) {
 			return PRODUCT_TWO;
-		} else if (type == 3){
-			return PRODUCT_THREE;
-		} else if (type == 4){
-			return PRODUCT_FOUR;
-		} else if (type == 5){
-			return PRODUCT_FIVE;
 		} else {
-			return PRODUCT_SIX;
-		} 
+			return PRODUCT_THREE;
+		}
 			
 	}
 
